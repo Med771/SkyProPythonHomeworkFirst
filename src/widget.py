070516a456539054bb82f36440980631491f459b
Med771 <mm7737333@gmail.com>
2024-10-28 11:07:02 +0300

    Begin develop

---
+++ src/widget.py
@@ -0,0 +1,39 @@
+from time import strftime, strptime
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card_info: str) -> str:
+    info_values = card_info.split()
+
+    if len(info_values) == 3:
+        return f"{info_values[0]} {info_values[1]} {get_mask_card_number(info_values[2])}"
+
+    if len(info_values) == 2:
+        return f"{info_values[0]} {get_mask_account(info_values[1])}"
+
+    return ""
+
+
+def get_date(date: str) -> str:
+    reformatted = strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+
+    return strftime("%d.%m.%Y", reformatted)
+
+
+if __name__ == "__main__":
+    data_info_cards = [
+        "Maestro 1596837868705199",
+        "Счет 64686473678894779589",
+        "MasterCard 7158300734726758",
+        "Счет 35383033474447895560",
+        "Visa Classic 6831982476737658",
+        "Visa Platinum 8990922113665229",
+        "Visa Gold 5999414228426353",
+        "Счет 73654108430135874305",
+    ]
+
+    print(get_date("2024-03-11T02:26:18.671407"))
+
+    for card in data_info_cards:
+        print(mask_account_card(card_info=card))
