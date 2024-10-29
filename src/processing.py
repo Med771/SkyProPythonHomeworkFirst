67a3cffd4726b4c2033c59ad9a850091b36720a0
Med771 <mm7737333@gmail.com>
2024-10-29 08:38:03 +0300

    homework 10_2 done Create tests for: src\masks.py src\widget.py src\processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,9 +6,15 @@ def filter_by_state(requests: list[dict], state: str = "EXECUTED") -> list[dict]
     Создаёт filter_transaction_req, а после заносит данные из requests, в которых state равны друг другу.
     Возвращает filter_transaction_req"""
 
+    if not isinstance(requests, list):
+        return []
+
     filter_transaction_req: list[dict] = []
 
     for request in requests:
+        if not isinstance(request, dict):
+            continue
+
         if request.get("state", "") == state:
             filter_transaction_req.append(request)
 
@@ -20,29 +26,24 @@ def sort_by_date(requests: list[dict], reverse: bool = True, format_: str = "%Y-
     Сортирует requests по дате с выбранным значением сортировки.
     Возвращает requests типа list[dict]."""
 
+    if not isinstance(requests, list):
+        return []
+
     def compare(request: dict) -> float:
+        if not isinstance(request, dict):
+            return time()
+
         if "date" in request:
             time_: str = request["date"]
 
-            return mktime(strptime(time_, format_))
+            if not isinstance(time_, str):
+                return time()
+
+            try:
+                return mktime(strptime(time_, format_))
+            except ValueError:
+                return time()
 
         return time()
 
     return sorted(requests, reverse=reverse, key=compare)
-
-
-if __name__ == "__main__":
-    transaction_requests = [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
-
-    print(*filter_by_state(transaction_requests), sep="\n")
-    print()
-    print(*filter_by_state(transaction_requests, state="CANCELED"), sep="\n")
-    print()
-    print(*sort_by_date(transaction_requests), sep="\n")
-    print()
-    print(*sort_by_date(transaction_requests, reverse=False), sep="\n")
--- src/widget.py
+++ src/widget.py
@@ -3,19 +3,26 @@ from time import strftime, strptime
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_info: str) -> str:
+def mask_account_card(account_card: str) -> str:
     """Принимает информацию о карте типа str в форматах Card Type Number / Account Number.
-    В зависимости от ответа, возвращает str в форматах Card Type .... ..** **** .... / Account **...."""
+    В зависимости от ответа, возвращает str в форматах Card Type .... .. **** .... / Account ...."""
 
-    info_values = card_info.split()
+    if not isinstance(account_card, str):
+        return "Account card type incorrect"
 
-    if len(info_values) == 3:
-        return f"{info_values[0]} {info_values[1]} {get_mask_card_number(info_values[2])}"
+    account_card.strip()
 
-    if len(info_values) == 2:
-        return f"{info_values[0]} {get_mask_account(info_values[1])}"
+    info_values = account_card.split()
 
-    return ""
+    if len(info_values) > 1:
+        number = info_values[-1]
+
+        if len(number) == 16:
+            return f"{' '.join(info_values[:-1])} {get_mask_card_number(number)}"
+        if len(number) == 20:
+            return f"{' '.join(info_values[:-1])} {get_mask_account(number)}"
+
+    return "Account card unknown"
 
 
 def get_date(date: str) -> str:
@@ -32,19 +39,3 @@ def get_date(date: str) -> str:
         return "Date format incorrect"
 
     return strftime("%d.%m.%Y", reformatted)
-
-
-if __name__ == "__main__":
-    data_info_cards = [
-        "Maestro 1596837868705199",
-        "Счет 64686473678894779589",
-        "MasterCard 7158300734726758",
-        "Счет 35383033474447895560",
-        "Visa Classic 6831982476737658",
-        "Visa Platinum 8990922113665229",
-        "Visa Gold 5999414228426353",
-        "Счет 73654108430135874305",
-    ]
-
-    for card in data_info_cards:
-        print(mask_account_card(card_info=card))
